ca91e4be808dad0ecdf54b2103286b7762f65c87
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-05-21 15:29:51 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_LP2
+{
+    class Aluno
+    {
+        string nome;
+        int anoNasc;
+        string tel;
+        double nota1C;
+        double nota2C;
+        double nota3C;
+    }
+}
---
+++ Programexercicio2105.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_LP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno al = new Aluno ();
+
+            Console.WriteLine("nome: ");
+            al.nome = Console.ReadLine();
+
+            Console.WriteLine("Ano Nascimento: ");
+            al.anoNasc = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Telefone: ");
+            al.tel = Console.ReadLine();
+
+            Console.WriteLine("Nota 1 cert: ");
+            al.nota1C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota 2 cert: ");
+            al.nota2C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota 3 cert: ");
+            al.nota3C = double.Parse(Console.ReadLine());
+
+
+            Aluno al2 = new Aluno();
+
+            Console.WriteLine("nome: ");
+            al2.nome = Console.ReadLine();
+
+            Console.WriteLine("Ano Nascimento: ");
+            al2.anoNasc = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Telefone: ");
+            al2.tel = Console.ReadLine();
+
+            Console.WriteLine("Nota 1 cert: ");
+            al2.nota1C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota 2 cert: ");
+            al2.nota2C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota 3 cert: ");
+            al2.nota3C = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
