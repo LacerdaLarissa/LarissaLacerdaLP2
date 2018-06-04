04e0494ccbd95e0ee1eaffa4c54be968a5d0eb22
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-06-04 14:58:25 -0300

    Add files via upload

---
+++ Cachorro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Cachorro
+    {
+        public string dono;
+        public string nome;
+        public string raça;
+        public string brinquedo;
+        public char sexo;
+        public double peso;
+        public bool pedigree;
+
+    }
+}
