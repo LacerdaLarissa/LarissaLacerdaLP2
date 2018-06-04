ec0f4043bdef65e04c16f7af4c56d3022ea07d11
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-06-04 15:00:38 -0300

    Add files via upload

---
+++ Programcachorro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cachorro c1 = new Cachorro();
+
+            c1.dono = "Larissa";
+            c1.nome = "Billy";
+            c1.raça = "vira-lata";
+            c1.brinquedo = "ladrão";
+            c1.sexo = 'M';
+            c1.peso = 8.000 ;
+            c1.pedigree = false;
+
+            Console.WriteLine("O cachorro {0} está brincando com um(a) {1}", c1.nome, c1.brinquedo);
+
+
+        }
+    }
+}
