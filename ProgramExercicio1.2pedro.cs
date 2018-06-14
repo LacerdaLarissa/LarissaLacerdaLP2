58256ab223a47569c32ae8d493e5b79feb90c64b
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-06-14 18:30:05 -0300

    Create ProgramExercicio1.2pedro.cs

---
+++ ProgramExercicio1.2pedro.cs
@@ -0,0 +1,38 @@
+int n = int.Parse(Console.ReadLine());
+            string modelo;
+            double km;
+            double pot;
+            string classVeic = "";
+            string classPot = "" ;
+            for(int i = 0; i < n; i++)
+            {
+                modelo = Console.ReadLine();
+                km = double.Parse(Console.ReadLine());
+                pot = double.Parse(Console.ReadLine());
+            
+                if (km <= 5000)
+                {
+                    classVeic = "Novo";
+                }
+                else if (km > 5000 && km <= 25000)
+                {
+                    classVeic = "Seminovo";
+                }
+                else
+                {
+                    classVeic = "Velho";
+                }
+                if (pot > 170)
+                {
+                    classPot = "Potente";
+                }
+                else if (pot >= 80 && pot <= 170)
+                {
+                    classPot = "Médio";
+                }
+                else
+                {
+                    classPot = "Popular";
+                }
+                Console.WriteLine("{0}-{1}-{2}\n", modelo, classVeic, classPot);
+            }
