425d7af8be30531208b1f3175809f52f4bf69e7c
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-06-14 18:48:38 -0300

    Create Carro.cs

---
+++ Carro.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Carro
+    {
+        static void Main(string[] args)
+        {
+          public double KmRodado;
+          public double Potencia;
+          public string Modelo;
+          
+          public static string Classificar (string ClassificTemp, string ClassificPot)
+          {
+            string Resultado= Modelo + ClassificTemp + ClassificPot;
+            if(KmRodado>=5000)
+             ClassificTemp="Novo";
+          
+            else if (KmRodado<5000 && KmRodado<=25000)
+            ClassificTemp="Seminovo";
+          
+            else //(KmRodado>25000)
+            ClassificTemp="Velho";
+          
+            if (Potencia<80)
+            ClassificPot="Popular";
+          
+            else if(Potencia>=80 && Potencia<170)
+           ClassificPot"Médio";
+          
+            else
+            ClassificPot="Potente";
+            
+            Console.WriteLine("{0}", Resultado);
+            return Classificar;
+          }
+        
+        }
+    }
+}
