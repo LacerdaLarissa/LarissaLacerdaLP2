33334724224de225f6bb95a6de4c2951fe2bcc4e
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-06-14 17:26:39 -0300

    Create Exercicio2.cs

---
+++ Exercicio2.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Veiculo
+    {
+        static void Main(string[] args)
+        {
+          int N=Console.ReadLine();
+          Double [] qtd = new double [N];
+          
+          string [] Modelo = New string [N];
+          string Resultado, ClassificTemp, ClassificPot;
+          double [] KmRodado= new double [N];
+          double [] Potencia= new double [N];
+          int i;
+          
+          for(i=0; i<N; i++)
+          {
+            Modelo[i]=Console.ReadLine();
+            KmRodado [i]= double.Parse(C.RL());
+            Potencia [i]= double.Parse(C.RL
+            if(KmRodado[i]<=5000)
++           ClassificTemp="Novo";
++          
++           else if (KmRodado[i]>5000 && KmRodado[i]<=25000)
++           ClassificTemp="Seminovo";
++          
++           else //(KmRodado[i]>25000)
++           ClassificTemp="Velho";
++          
++           if (Potencia[i]<80)
++           ClassificPot="Popular";
++          
++           else if(Potencia[i]>=80 && Potencia<170)
++           ClassificPot"Médio";
++          
++           else
++           ClassificPot="Potente";
+          }
+        
+            Console.writeLine("{0}-{1}-{2}",Modelo[i], ClassificTemp, ClassificPot);
+        }
+    }
+}
