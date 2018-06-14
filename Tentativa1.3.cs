8051a274d9ac687483e9c4df5fb45febc02f166c
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-06-14 18:49:39 -0300

    Create Tentativa1.3.cs

---
+++ Tentativa1.3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int N= int.Parse(Console.ReadLine());
+          Carro c= new Carro();
+
+          for (i=0; i<N; i++)
+          {
+            c.Modelo = Console.ReadLine();
+            c.Potencia= double.Parse(Console.ReadLine());
+            cKmRodado= double.Parse(Console.ReadLine());
+          }
+           Console.WriteLine("{0}\N", c.Classificar());
+        }
+    }
+}
