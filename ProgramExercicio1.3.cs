47c2e9ab211fede131678266ab6c2de714a3dc31
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-07-04 20:01:01 -0300

    Create ProgramExercicio1.3.cs

---
+++ ProgramExercicio1.3.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Veiculo
+    {
+        static void Main(string[] args)
+        {   
+            int n = int.Parse(C.RL());
+             for (int i = 0; i < n; i++)
+             {
+                C.WL(Classificar(car));
+             }
+        }
+        static string Classificar(Carro c)
+        {
+            string ClassVeic;
+            string ClassPot;
+            
+            if (c.km <= 5000) 
+            ClassVeic = "Novo";
+                
+            else if (c.km > 5000 && c.km <= 25000)
+            ClassVeic = "Seminovo";
+                
+            else//km > 25000
+            ClassVeic = "Velho";
+                
+            if (c.pot < 80)
+            ClassPot = "Popular
+                    
+            else if(c.pot >= 80 && c.pot < 170)
+            ClassPot="Médio";
+                
+            else//pot > 170
+            ClassPot="Potente";
+          
+            
+            return string.Format("{0} - {1} - {2}", c.modelo, ClassVeic, ClassPot);
+        }
+    }
+}
