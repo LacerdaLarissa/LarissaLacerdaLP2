e8d30d3ac39d57dae181ddb496e3ed2ba7cfa6de
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-07-04 20:08:52 -0300

    Update ProgramExercicio1.1.cs

--- ProgramExercicio1.1.cs
+++ ProgramExercicio1.1.cs
@@ -13,7 +13,7 @@ namespace ConsoleApplication4
           string modelo, ClassVeic= "", ClassPot= "";
           double km, pot;
           
-          modelo=C.RL();
+          modelo = C.RL();
           km = double.Parse(C.RL());
           pot = double.Parse(C.RL());
           
@@ -23,7 +23,7 @@ namespace ConsoleApplication4
           else if (km > 5000 && km <= 25000)
           ClassVeic="Seminovo";
           
-          else //(km > 25000)
+          else //km > 25000
           ClassVeic="Velho";
           
           if (pot < 80)
@@ -32,7 +32,7 @@ namespace ConsoleApplication4
           else if(pot >= 80 && pot < 170)
           ClassPot="Médio";
           
-          else//km > 170
+          else//pot > 170
           ClassPot="Potente";
           
           Console.WriteLine("{0} - {1} - {2}", modelo, ClassVeic, ClassPot);
