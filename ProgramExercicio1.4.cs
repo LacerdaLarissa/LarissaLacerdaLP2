dea58c5e32d288f1d789f93b38fff03cbc86e45e
LacerdaLarissa <38438391+LacerdaLarissa@users.noreply.github.com>
2018-07-04 20:00:15 -0300

    Update ProgramExercicio1.4.cs

--- ProgramExercicio1.4.cs
+++ ProgramExercicio1.4.cs
@@ -15,41 +15,37 @@ namespace ConsoleApplication4
             for (int i = 0; i < n; i++)
             {
                 Carro car = new Carro();
-                car.modelo = Console.ReadLine();
-                car.km = double.Parse(Console.ReadLine());
-                car.pot = double.Parse(Console.ReadLine());
-                Console.WriteLine(Classificar(car));
+                car.modelo = C.RL();
+                car.km = double.Parse(C.RL());
+                car.pot = double.Parse(C.RL());
+                C.WL(Classificar(car));
             }
         }
         static string Classificar(Carro c)
         {
-            string classVeic;
-            string classPot;
-            if (c.km <= 5000)
-            {
-                classVeic = "Novo";
-            }
+            string ClassVeic;
+            string ClassPot;
+            
+            if (c.km <= 5000) 
+            ClassVeic = "Novo";
+                
             else if (c.km > 5000 && c.km <= 25000)
-            {
-                classVeic = "Seminovo";
-            }
-            else
-            {
-                classVeic = "Velho";
-            }
-            if (c.pot > 170)
-            {
-                classPot = "Potente";
-            }
-            else if (c.pot >= 80 && c.pot <= 170)
-            {
-                classPot = "Médio";
-            }
-            else
-            {
-                classPot = "Popular";
-            }
-            return string.Format("{0}-{1}-{2}", c.modelo, classVeic, classPot);
+            ClassVeic = "Seminovo";
+                
+            else//km > 25000
+            ClassVeic = "Velho";
+                
+            if (c.pot < 80)
+            ClassPot = "Popular
+                    
+            else if(c.pot >= 80 && c.pot < 170)
+            ClassPot="Médio";
+                
+            else//pot > 170
+            ClassPot="Potente";
+          
+            
+            return string.Format("{0} - {1} - {2}", c.modelo, ClassVeic, ClassPot);
         }
     }
 }
